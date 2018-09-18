375384d30ed5d70b02a0fa434323a97adec32d76
ahmedkhattab <ahmedkhattab@aa0508-24.labs.sce.carleton.ca>
2018-09-18 12:09:52 -0400

    buddyinfo

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,4 +1,29 @@
+import java.util.*;
 
 public class AddressBook {
+	
+	private ArrayList<BuddyInfo> buddy;
+	
 
+	public AddressBook(){
+		this.buddy = new ArrayList<>();
+	}
+	
+	public void addBuddy(BuddyInfo buddyInfo) {
+		if(buddyInfo != null) {
+			buddy.add(buddyInfo);
+		}
+	}
+
+	public void removeBuddy(int index) {
+		if(index >=0 && index< buddy.size()) {
+			buddy.remove(index);
+		}
+	}
+public static void main(String[] args) {
+		
+		System.out.println("Address Book");
+
+	}
+	
 }
